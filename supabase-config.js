69060fb813c70ff0ae34cb957cb4d8c7a7826394
12411136-giteacher <12411136@sankogakuen.jp>
2026-06-17 17:29:24 +0900

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-window.ITPWA_SUPABASE = {
-  url: "https://abegheguaoguuqhawfgt.supabase.co",
-  publishableKey: "sb_publishable_0lDhTxl_KBJ5ozpQTHqjxw_R3fxbwIH"
-};
-
-window.supabaseBackendReady = new Promise((resolve) => {
-  window.resolveSupabaseBackend = resolve;
-});
